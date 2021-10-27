bf5dd2b56e4b51974dd91038a4230b0271e4825e
Lcofili <lcofili2000@gmail.com>
2021-10-27 17:22:54 +0100

    updated push

--- src/Lucky/About.js
+++ src/Lucky/About.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import styled from 'styled-components';
-import pic from '../Lucky/AboutUI.png'
+// import pic from '../Lucky/AboutUI.png'
 import pix from '../Lucky/hotelimages/About.jpg'
+import pics from '../Lucky/hotelimages/AboutCard.jpg'
+import pixs from '../Lucky/hotelimages/AboutCard1.jpg'
+import pices from '../Lucky/hotelimages/AboutCard2.jpg'
+import imag from '../Lucky/hotelimages/AboutCard8.jpg'
+import ima from '../Lucky/hotelimages/AboutCard3.jpg'
+import imagi from '../Lucky/hotelimages/AboutCard5.jpg'
 
 const About = () => {
     return (
         <AboutContainer>
             {/* <Logo src={pic} /> */}
-            <Wrapper>
             About Us
-
+            <Wrapper>
                 <WrapHold>
                 <Text>
                 Connecting with like-minded thinkers and collaborating with members from similar (or different) industries results in a more progressive and successful work environment.
@@ -21,55 +26,110 @@ const About = () => {
                 </Text>
                 <Image img src={pix} />
                 </WrapHold>
-                <OtherCard>
-                    What We Offer
-                    <Card>
-                        <span>Page1</span>
-                        <span>Page2</span>
-                        <span>Page3</span>
-                    </Card>
-                    {/* <Card>page 2</Card> */}
-                    {/* <Card>page 3</Card> */}
-                </OtherCard>
             </Wrapper>
+            <OtherCard>
+                    What We Offer
+                </OtherCard>
+                <CardHolder>
+                <Card src={ pics} />
+                    <Card1 src={ pixs}/>
+                    <Card2 src={ pices}/>   
+                </CardHolder>
+
+            <FindCard>
+                    Find Your Place
+                </FindCard>
+                <FindHolder>
+                Complete Comfort
+                    <Find src={ imag} />
+                        Everything Luxury
+                    <Find src={ ima} />
+                    Sweet Time
+                    <Find src={ imagi} />
+                </FindHolder>
+
+        <HoldFind>
+        <LastCard>Dicover</LastCard>
+        </HoldFind>
+
         </AboutContainer>
     )
 }
 
 export default About;
 
+const LastCard = styled.div`
+Width: 150px;
+height: 100px;
+background-color: #ff9200;
+display: flex;
+justify-content: center;
+align-items: center;
+`
+
 const AboutContainer = styled.div`
-width: 100vw;
-height: 100%;
+width: 100%;
+// height: 100vh;
 font-size: 25px;
 font-weight: bold;
 // color: black;
 display: flex;
+justify-content: center;
 flex-wrap: wrap;
-justify-content: space-between;
-
+// justify-content: space-between;
 `;
 const Wrapper = styled.div`
 width:100%;
-height: 100vh;
+// height: 100%;
 color: gray;
 display: flex;
 justify-content: center;
+align-items: center;
+margin-top: 40px;
+
 `;
-// const Logo = styled.img`
+const Find = styled.img`
 // object-fit: cover;
 // width: 350px;
 // height: 350px;
 // // border-radius: 30px;
-// color: #FFFFFF
-// `;
+// // color: #FFFFFF
+width: 400px;
+height: 400px;
+background-color: #ff9200;
+margin: 70px;
+object-fit: cover;
+justify-content: space-between;
+align-items: center;
+`;
+
+const FindHolder = styled.div`
+width: 70%;
+display: flex;
+justify-content: space-around;
+align-items: center;
+`
+
+const FindCard = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+margin-top: 40px;
+`
+
+
+const HoldFind = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+`;
 
 const WrapHold = styled.div`
+width: 70%;
 display: flex;
-justify-content: space-between;
-// flex-direction: row;
+justify-content: space-around;
 align-items: center;
-padding-bottom: 120px;
+// padding-bottom: 120px;
 `;
 const Text = styled.div`
 width: 350px;
@@ -92,16 +152,46 @@ justify-content: center;
 `;
 const OtherCard = styled.div`
 display: flex;
-justify-content: space-between;
-// align-items: center;
+justify-content: center;
+align-items: center;
+margin-top: 40px;
 
 `;
-const Card = styled.div`
+const Card = styled.img`
 width: 400px;
 height: 400px;
 background-color: #ff9200;
 margin: 70px;
+object-fit: cover;
 justify-content: space-between;
+align-items: center;
+
+`;
+
+const Card1 = styled.img`
+width: 400px;
+height: 400px;
+background-color: #ff9200;
+margin: 70px;
+object-fit: cover;
+justify-content: space-between;
+align-items: center;
+
+`;
+
+const Card2 = styled.img`
+width: 400px;
+height: 400px;
+background-color: #ff9200;
+margin: 70px;
+object-fit: cover;
+justify-content: space-between;
+align-items: center;
+`;
+
+const CardHolder = styled.div`
+display: flex;
+justify-content: center;
 align-items: center;
 
 `;
